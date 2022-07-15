b833bab2e2a93ef39d594e19f184ddecea6562a2
DanielValeriote <danielvaleriote@outlook.com>
2022-07-14 23:11:00 -0300

    change User model, create /register endpoint

--- back-end/src/Models/User.js
+++ back-end/src/Models/User.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	createdAt: {
-		type: Date,
-    immutable: true,
-    default: () => Date.now()
-	},
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	refreshToken: { type: String, required: false, default: ""},
+	createdAt: { type: Date, immutable: true, default: () => Date.now() },
 });
 
 module.exports = mongoose.model("User", schema);
--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./Models/User");
+const {hash} = require("bcryptjs");
 require("dotenv").config();
 const app = express();
 
@@ -12,5 +13,23 @@ const port = process.env.PORT || 3500;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.post("/register", async (req, res) => {
+	console.log(await User.find({}))
+	const {username, password} = req.body;
+
+	if(!username || !password) return res.sendStatus(422);
+
+	try {
+		const user = await User.create({username, password: await hash(password, 10)});
+		res.status(201).json({msg: "User created with success."});
+
+	} catch(err) {
+			console.error(err.message);
+
+			if(err.code == 11000) return res.status(409).json({msg:"Username already registered."});
+			res.sendStatus(500);
+	};
+})
 
 app.listen(port, () => console.info(`Server is listening to ${port}.`));
